32dc09d33127ac3580898ee2741fbeb6a02e4ad5
nishgup2005 <nishgup2005@gmail.com>
2025-07-01 09:13:41 +0530

    added login and registration capabilities

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 from typing import Annotated
-from fastapi import FastAPI, HTTPException, Depends
-from fastapi.responses import JSONResponse
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException, Depends, Request, Response
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse, PlainTextResponse
+from pydantic import BaseModel, EmailStr, ValidationError, field_validator, Field
 import model
 from database import engine, get_db
 from sqlalchemy.orm import session
-from datetime import datetime
+from datetime import datetime, timedelta
+from passlib.context import CryptContext
+from secrets import token_hex
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import jwt, JWTError
 
 app = FastAPI()
 model.Base.metadata.create_all(bind=engine)
-
+secret_key = token_hex(32)
+hash = 'HS256'
+time_to_live = 30
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')
 
 #custom class to validate user information
 class UserBase(BaseModel):
 
     name: str
-    email: str
+    email: EmailStr
     password: str
     phone_no: str
+    role : str = "user"
 
 
 #custom class to validate salary information 
@@ -36,12 +46,57 @@ class InputBase(BaseModel):
     value: str | int | bool | datetime
 
 
+class LoginBase(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class RegisterBase(BaseModel):
+    name: str
+    email: EmailStr
+    password: str = Field(min_length=10)
+    phone_no: str = ""
+    role : str = "user"
+
+    @field_validator('password', mode='after')
+    @classmethod
+    def validate_password(cls, value:str):
+        uprcnt=0
+        lwrcnt=0
+        spclcnt=0
+        for i in value:
+            if i.isupper():
+                uprcnt+=1
+            if i.islower():
+                lwrcnt+=1
+            if not i.isalnum():
+                spclcnt+=1
+        if not(uprcnt >= 1):
+            raise ValueError("password must have atleast 1 uppercase letter")
+        if not(lwrcnt >= 1):
+            raise ValueError("password must have atleast 1 lowercase letter")
+        if not(spclcnt >= 1):
+            raise ValueError("password must have atleast 1 special character")
+        
+        return value
+    
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
 db_dependency = Annotated[session, Depends(get_db)]
+
+
+# @app.exception_handler(RequestValidationError)
+# def validation_exception_handler(request, exc):
+#     print(exc)
+#     return {"status_code":422,
+#     "msg":exc},
 
 
 # Landing Page
 @app.get("/")
-def read_root():
+async def read_root():
     return {"msg":"Welcome to User Manipulation Backend"}
 
 
@@ -68,7 +123,6 @@ async def get_user(db: db_dependency):
     return JSONResponse(content={"status_code": 200,
                                  "users": users,
                                  "msg": f"found {len(results)} user(s)"},
-
                         status_code=200)
 
 # GET:/user returns the User whos id matches with the input user_id
@@ -116,15 +170,102 @@ async def get_user_id(user_id: int, db: db_dependency):
 # After inserting user data it is important to call POST:/salary
 # as well for the inserted user-id
 
+# @app.post('/login',status_code=200)
+# async def user_login(user: LoginBase, db: db_dependency):
+
+#     email = user.email
+#     password = user.password
+#     cUser = (db.query(model.Users)
+#             .filter(model.Users.email==email)
+#             .first())
+
+#     if (password == cUser[0].password):
+#         return JSONResponse(content={"status_code":200,
+#                                      "msg":"Login Successful"
+#                                      },
+#                             headers={"x-user-token":cUser[0].id},
+#                             status_code=200)
+
+#     else:
+#         return JSONResponse(content={"status_code":401,
+#                                      "msg":"Incorrect Username/Password"},
+#                             status_code=401)
+
+
+@app.post('/register',status_code=201)
+async def user_register(user:RegisterBase, db:db_dependency):
+    try:
+        db_user = model.Users(name=user.name,
+                            email=user.email,
+                            password=bcrypt_context.hash(user.password),
+                            phone_no=user.phone_no,
+                            role=user.role)
+
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
+
+
+    except Exception as e:
+        msg=str(e.orig).split(':')[-1].replace('\n', '').strip()
+        return JSONResponse(content={"status_code": 422,
+                                     "msg": "Unprocessable Entity",
+                                     "detail":msg},
+                            status_code=422)
+
+    return JSONResponse(content={"status_code":201,
+                                 "user_id": db_user.id,
+                                 "msg": f"user {db_user.name} "
+                                 "has been added successfully."},
+                        status_code=201)
+
+
+@app.post('/login',response_model=Token)
+async def token_login(form_data:Annotated[OAuth2PasswordRequestForm,Depends()],db:db_dependency):
+    print(form_data.username, form_data.password,sep=" | ")
+    user = authenticateUser(form_data.username, form_data.password, db)
+
+    if not user:
+        return JSONResponse(content={"status_code":401,
+                                     "msg":"Unauthorized",
+                                     "detail":"invalid username/password"},
+                            status_code=401)
+    
+    token = create_user_token(user.email, user.id, timedelta(minutes=time_to_live))
+    return JSONResponse(content={"status_code":200,
+                                 "msg":"Login Successful"},
+                        headers={"token":token},
+                        status_code=200)
+
+
+def authenticateUser(email: EmailStr, password:str, db:db_dependency) -> model.Users:
+    user = (db.query(model.Users)
+            .filter(model.Users.email==email)
+            .first())
+    if not user:
+        return False
+    
+    if not bcrypt_context.verify(password, user.password):
+        return False
+    
+    return user
+
+def create_user_token(email:str, user_id:int, ttl:timedelta):
+    encode={"sub":email, "id":user_id}
+    expires = datetime.now()+ttl
+    encode.update({"exp":expires})
+    print(jwt.encode(encode, secret_key, algorithm=hash))
+    return jwt.encode(encode, secret_key, algorithm=hash)
+
 
 @app.post('/user', status_code=201)
 async def create_User(user: UserBase, db: db_dependency):
     try:
-
         db_user = model.Users(name=user.name,
-                            email=user.email,
-                            password=user.password,
-                            phone_no=user.phone_no)
+                              email=user.email,
+                              password=user.password,
+                              phone_no=user.phone_no,
+                              role=user.role)
 
         db.add(db_user)
         db.commit()
@@ -268,11 +409,6 @@ def update_salary(input:InputBase, db: db_dependency):
     value = input.value
     invalid_input=False
 
-    # if field == "id":
-    #     return JSONResponse(content={"status_code": 403,
-    #                                  "msg": "Forbidden",
-    #                                  "detail": "Changing the ID for salary is Forbidden"},
-    #                                  status_code=403)
     
     if field not in ["salary","user_id"] and not isinstance(value,int):
         invalid_input=True
--- model.py
+++ model.py
@@ -9,9 +9,10 @@ class Users(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, nullable=False, index=True)
     name = Column(String, nullable=False)
-    email = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     phone_no = Column(String,  nullable=False)
+    role = Column(String, nullable=False)
     salary = relationship("Salary", back_populates="user", cascade="all, delete-orphan")
 
 
